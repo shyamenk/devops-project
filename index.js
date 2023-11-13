b350e10d3d212d6156ef839159c1ac74cce80e9e
shyam <shyamenk@gmail.com>
2023-11-13 18:48:14 +0000

    create dev and prod environment dokcer-compose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hello therddde!👋</h2>");
+  res.send("<h2>Docker Hub</h2>");
 });
 
 const PORT = process.env.PORT || 3000;
