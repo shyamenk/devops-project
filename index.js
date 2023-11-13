dd1611b3cb9389123e912fe56343b4a4e79d4d27
shyam <shyamenk@gmail.com>
2023-11-13 16:18:04 +0000

    Dockerfile Added.🐟

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hello there!🙋‍♂️</h2>");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => console.log(`Server listening on port ${PORT}`));
