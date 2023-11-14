ac75c7e3035d80803045c5b4d3b6ecb82da2a16a
shyam <shyamenk@gmail.com>
2023-11-14 14:12:47 +0000

    added post, user auth features

---
+++ controllers/authController.js
@@ -0,0 +1,71 @@
+const User = require("../models/userModel");
+
+const bcrypt = require("bcryptjs");
+
+exports.signUp = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Username already exists",
+      });
+    }
+
+    const hashpassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      username,
+      password: hashpassword,
+    });
+
+    // req.session.user = newUser;
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      status: "fail",
+      message: e.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  console.log(username, password);
+  try {
+    const user = await User.findOne({ username });
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({
+        status: "fail",
+        message: "user not found",
+      });
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+    console.log(isCorrect);
+    if (isCorrect) {
+      // req.session.user = user;
+      res.status(200).json({
+        status: "Login success",
+      });
+    } else {
+      res.status(400).json({
+        status: "fail",
+        message: "incorrect username or password",
+      });
+    }
+  } catch (e) {
+    res.status(400).json({
+      status: "fail",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const connectDB = require("./db");
 const app = express();
 
+// app.use(
+//   session({
+//     secret: "gfesghhyht",
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
+const postRouter = require("./routes/postRoutes.js");
+const userRouter = require("./routes/userRoutes");
+
 connectDB();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/api/v1", (req, res) => {
   res.send("<h2>Connected to MongoDB</h2>");
 });
 
-const PORT = process.env.PORT || 3000;
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
-app.listen(3000, () => console.log(`Server listening on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
